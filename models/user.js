1bc4a0be0d414bf5a78054d2cb1331f2b9efc6c1
Hydroxion <whenrique26@gmail.com>
2019-07-15 02:28:22 -0300

    Remove birth and gender from user model

--- models/user.js
+++ models/user.js
@@ -24,8 +24,6 @@ export default (sequelize, DataTypes) => {
         type: DataTypes.STRING(8),
         unique: true
       },
-      gender: DataTypes.CHAR(1),
-      birth: DataTypes.DATEONLY,
       firstName: DataTypes.STRING,
       lastName: DataTypes.STRING
     },
