feb448cb5cc164de6f20749034be011cab3ea1ef
Hydroxion <whenrique26@gmail.com>
2019-07-15 03:17:58 -0300

    Yarn dependencies updated and base user schema defined

--- index.js
+++ index.js
@@ -8,33 +8,63 @@ const express = require("express");
 
 const app = express();
 
+let users = [];
+
 app.use(
   "/graphql",
   graphQlHTTP({
     schema: buildSchema(`
-        type RootQuery {
-            events: [String!]!
-        }   
+      type User {
+        id: Int
+        email: String!
+        password: String!
+        cpf: String
+        matriculation: String!
+        firstName: String!
+        secondName: String!
+      }
 
-        type RootMutation {
-          createEvent(name: String): String
-        }
+      input UserInput {
+        email: String!
+        password: String!
+        cpf: String
+        matriculation: String!
+        firstName: String!
+        secondName: String!
+      }
 
-        schema {
-            query:
-                RootQuery
-            mutation:
-                RootMutation
-        }
+      type RootQuery {
+        user: [User!]!
+      }   
+
+      type RootMutation {
+        createUser(userInput: UserInput): User
+      }
+
+      schema {
+        query:
+            RootQuery
+        mutation:
+            RootMutation
+      }
     `),
     rootValue: {
-      events: () => {
-        return ["test1", "test2"];
+      user: () => {
+        return users;
       },
-      createEvent: args => {
-        const eventName = args.name;
+      createUser: args => {
+        const user = {
+          email: args.userInput.email,
+          password: args.userInput.password,
+          cpf: args.userInput.cpf,
+          matriculation: args.userInput.matriculation,
+          firstName: args.userInput.firstName,
+          secondName: args.userInput.secondName
+        };
 
-        return eventName;
+        users.push(user);
+
+        return user;
       }
     }, // Resolvers
     graphiql: true,
