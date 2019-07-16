503ec7b0fcc20f688be5f2c431af86bb67952a01
Hydroxion <whenrique26@gmail.com>
2019-07-15 23:58:27 -0300

    Generate admin on database synchronization

--- sync.js
+++ sync.js
@@ -1,8 +1,21 @@
 const db = require("./models");
 
+const bcryptjs = require("bcryptjs");
 const syncDb = async () => {
   try {
     await db.sequelize.sync();
+
+    const hashedPassword = await bcryptjs.hash("!-", 12);
+
+    db.user.create({
+      email: "admin@gmail.com",
+      password: hashedPassword,
+      cpf: "00000000000",
+      matriculation: "1111",
+      firstName: "Admin",
+      secondName: "",
+      creator: 1
+    });
   } catch (error) {
     console.log(error);
   }
