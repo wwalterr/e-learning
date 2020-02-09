686b3a35a5f6c3bbc80a9316b5f6bd346b962e2a
Hydroxion <whenrique26@gmail.com>
2020-02-08 23:03:19 -0300

    Create contact, address and courses on setup

--- data.js
+++ data.js
@@ -14,16 +14,60 @@ const generateAdmin = async () => {
     const user = {
       email: "admin@gmail.com",
       password: hashedPassword,
-      cpf: "",
-      matriculation: "",
+      cpf: "92711239055",
+      matriculation: "4000",
       firstName: "Admin",
-      secondName: "",
+      secondName: "Management",
       creator: 1
+    };
+
+    const address = {
+      street: "Rua Benedito Correia Penha",
+      number: 101,
+      complement: "Floor 2",
+      city: "Vitória",
+      state: "Espírito Santo",
+      zipCode: "29120311",
+      userId: 1
+    };
+
+    const contact = {
+      email: "admin-backup@gmail.com",
+      phone: "34991634340",
+      userId: 1
+    };
+
+    const course1 = {
+      title: "Private",
+      description: "Learn how too play piano",
+      start: "2019-07-27T19:16:42.599Z",
+      end: "2019-07-27T19:16:57.509Z",
+      creator: 1,
+      private: true
+    };
+
+    const course2 = {
+      title: "Public",
+      description: "Learn how too play guitar",
+      start: "2019-07-27T19:16:42.599Z",
+      end: "2019-07-27T19:16:57.509Z",
+      creator: 1,
+      private: false
     };
 
     const userCreated = await db.user.create(user);
 
-    userCreated.update({ creator: userCreated.dataValues.id });
+    const addressCreated = await db.address.create(address);
+
+    const contactCreated = await db.contact.create(contact);
+
+    const coursesCreated = await db.course.bulkCreate([course1, course2]);
+
+    userCreated.update({
+      creator: userCreated.dataValues.id,
+      address: { ...addressCreated.dataValues },
+      contact: { ...contactCreated.dataValues }
+    });
 
     return userCreated;
   } catch (error) {
